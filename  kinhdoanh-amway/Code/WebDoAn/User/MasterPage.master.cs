07e47168abc68ee1b1e6ad27183d7afaee0c7cda
manhhung.it9x <hungnm92@users.noreply.github.com>
2014-04-22 03:32:27 +0000

    Chuẩn HD

---  kinhdoanh-amway/Code/WebDoAn/User/MasterPage.master.cs
+++  kinhdoanh-amway/Code/WebDoAn/User/MasterPage.master.cs
@@ -10,10 +10,11 @@ public partial class User_MasterPage : System.Web.UI.MasterPage
     webdoan.Menu mn = new webdoan.Menu();
     protected void Page_Load(object sender, EventArgs e)
     {
-        lblMenu.Text = mn.LoadMenu(Session["MaNPP"].ToString(), 0);
+        //lblMenu.Text = mn.LoadMenu(Session["MaNPP"].ToString(), 0);
+        lblMenu.Text = mn.LoadMenu("2976313", 0);
         //if (Session["MaNPP"] == null)
            // Response.Redirect("~/User/DangNhap.aspx");
-        //lblUser.Text = "Xin chào " + Session["HoTenNV"].ToString() + ".";
+        //lblUser.Text = "Xin chào " + Session["HoTenNPP"].ToString() + ".";
     }
 
 }
